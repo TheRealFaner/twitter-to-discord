10ca42f09d08fd247a1f59cba38f4295e2358bb9
TheRealFaner <faner.tuz@gmail.com>
2024-04-17 10:55:17 +0300

    First commit

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+export TWITTER_ACCOUNT="SomeAccName"
+export DISCORD_HOOK_LINK="https://discordapp.com/api/webhooks/123/qwe_asd"
+export TWITTER_USERNAME="twitterUserName"
+export TWITTER_PASSWORD="twitterPassword"
+export REPEAT_INTERVAL_SEC=10
+export MONGO_HOST="localhost"
+export MONGO_PORT=27017
+export MONDO_DBNAME="twitter-discord"
+py run.py
